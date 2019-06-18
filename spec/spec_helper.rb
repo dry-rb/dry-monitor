1cb804c75b9f28af74eeb34a4278d9c9129655a4
Piotr Solnica <piotr.solnica@gmail.com>
2019-06-18 21:04:33 +0200

    Update coverage setup on travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
-if RUBY_ENGINE == 'ruby' && ENV['CI'] == 'true'
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter '/spec/'
+# frozen_string_literal: true
+
+if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
+  require 'yaml'
+  rubies = YAML.safe_load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
+  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
+
+  if RUBY_VERSION == latest_mri
+    require 'simplecov'
+    SimpleCov.start do
+      add_filter '/spec/'
+    end
   end
 end
 
